c81974d36613c296da2549bbeaad920ebc98e34f
EbaraKoji <kojieva5270@gmail.com>
2024-06-27 15:29:06 +0900

    update base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gh-pages-react/',
+  base: process.env.GITHUB_PAGES ? 'gh-pages-react' : './',
   plugins: [react()],
   server: { port: 3000 },
 })
