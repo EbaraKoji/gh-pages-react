4e1c2239b7537d41da16e5df5825fe5c68ca415e
EbaraKoji <kojieva5270@gmail.com>
2024-06-27 15:19:22 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className="text-2xl text-red-800">Hello, my React GitHub Page!</h1>
+    </div>
+  )
+}
+
+export default App
